f6ea8a650ef783a0a38595cfa7443bf64990a900
Colin Dixon <dixikinz@gmail.com>
2020-06-18 17:15:35 +0100

    recompleting lessons

--- lib/main.dart
+++ lib/main.dart
@@ -18,12 +18,67 @@ class MyApp extends StatelessWidget {
                 radius: 50.0,
                 backgroundImage: AssetImage('images/head.png'),
               ),
-              Container(child: Text('Colin Dixon')),
-              Container(child: Text('Flutter Developer')),
-              Container(child: Text('colin@socialiseapp.co.uk')),
-              Container(child: Text('07511912104')),
               Container(
-                width: double.infinity),
+                child: Text(
+                  'Colin Dixon',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(child: Text('Flutter Developer',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                color: Colors.indigo.shade100,
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.indigo.shade100,
+              ),
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.indigo,
+                  ),
+                  title: Text(
+                    '+44 07511 912 104',
+                    style: TextStyle(
+                      color: Colors.indigo.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.indigo,
+                  ),
+                  title: Text(
+                    'dixikinz@gmail.com',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.indigo.shade900,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
